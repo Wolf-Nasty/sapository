c74865157d5febe8ddd278c23ac7f3be116bbf48
Wolf-Nasty Jade's bf <squidwolves@gmail.com>
2023-08-24 16:48:25 +0000

    lightyear

--- bigdawg.py
+++ bigdawg.py
@@ -1,1 +1,9 @@
-print ("Big Dawg")
+print ("Welcome to Rocketman Time")
+distance = int(input("How far do you want to go? (lightyears)"))3
+speed = float(input("How fast are you going? (percent speed of light)"))
+time = distance/speed
+print (time, "Lightyears/mps") 
+
+
+
+
